a2e32af49272e1ae5f479217e49fe1d5e0f1c5c4
morsy ramadan <morsyhashish123@gmail.com>
2024-07-09 06:10:57 +0300

    added spacing and some styles and colors and general textbitton and general textformfield and finish loginscreen design

--- lib/features/login/presentaion/ui/login_screen.dart
+++ lib/features/login/presentaion/ui/login_screen.dart
@@ -1,10 +1,92 @@
+import 'package:appointement_booking_app/core/helpers/spacing.dart';
+import 'package:appointement_booking_app/core/theming/styles.dart';
+import 'package:appointement_booking_app/core/widgets/app_text_button.dart';
+import 'package:appointement_booking_app/core/widgets/app_text_form_field.dart';
+import 'package:appointement_booking_app/features/login/presentaion/ui/widgets/terms_and_condition_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+import 'widgets/dont_have_any_account.dart';
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formkey = GlobalKey<FormState>();
+  bool isObscureText = true;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+        child: SingleChildScrollView(
+          child: Form(
+            key: formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last\n logged in.',
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+                Column(
+                  children: [
+                    AppTextFormField(
+                      hintText: 'Email',
+                      validator: (value) {},
+                    ),
+                    verticalSpace(10),
+                    AppTextFormField(
+                      isObscureText: isObscureText,
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isObscureText = !isObscureText;
+                          });
+                        },
+                        child: Icon(isObscureText
+                            ? Icons.visibility
+                            : Icons.visibility_outlined),
+                      ),
+                      hintText: 'Password',
+                      validator: (value) {},
+                    ),
+                    verticalSpace(20),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Forget Password?',
+                        style: TextStyles.font13BlueRegular,
+                      ),
+                    ),
+                    verticalSpace(20),
+                    AppTextButton(
+                      buttonText: 'Login',
+                      onPressed: () {},
+                      textStyle: TextStyles.font16WhiteMedium,
+                    ),
+                    verticalSpace(16),
+                    const TermsAndConditionsText(),
+                    verticalSpace(16),
+                    const DontHaveAccountText()
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      )),
+    );
   }
 }
